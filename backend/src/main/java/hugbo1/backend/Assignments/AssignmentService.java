bd81dd382f661cccbb382ebc72fed13fc750c3d6
Mkaldalons <mrk9@hi.is>
2024-10-19 20:51:45 +0000

    Assignment virkni í bakenda, vantar soldið í framendann

---
+++ backend/src/main/java/hugbo1/backend/Assignments/AssignmentService.java
@@ -0,0 +1,29 @@
+package hugbo1.backend.Assignments;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AssignmentService {
+
+    private final AssignmentRepository assignmentRepository;
+
+    public AssignmentService(AssignmentRepository assignmentRepository) {
+        this.assignmentRepository = assignmentRepository;
+    }
+
+    public void createAssignment(Assignment assignment) {
+        assignmentRepository.save(assignment);
+    }
+
+    public List<Assignment> getAllAssignments() {
+        return assignmentRepository.findAll();
+    }
+    public Assignment getAssignmentById(int id) {
+        return assignmentRepository.findById(id);
+    }
+    public void deleteAssignmentById(int id) {
+        assignmentRepository.deleteById(id);
+    }
+}
