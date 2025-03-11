433a87c0d12a34dd38a48a6d907eb16abc6d9382
Mkaldalons <mrk9@hi.is>
2025-03-11 18:19:19 +0000

    Skila AssignmentResponse en ekki Assignment

---
+++ backend/src/main/java/hugbo1/backend/Assignments/AssignmentResponse.java
@@ -0,0 +1,65 @@
+package hugbo1.backend.Assignments;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class AssignmentResponse {
+    private Integer assignmentId;
+    private LocalDate dueDate;
+    private List<QuestionRequest> questionRequest;
+    private Integer courseId;
+    private String assignmentName;
+    private Boolean published;
+
+    public Integer getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(Integer assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public Boolean getPublished() {
+        return published;
+    }
+
+    public void setPublished(Boolean published) {
+        this.published = published;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
+
+    public void setAssignmentName(String assignmentName) {
+        this.assignmentName = assignmentName;
+    }
+
+    public List<QuestionRequest> getQuestionRequest() {
+        return questionRequest;
+    }
+
+    public void setQuestionRequest(List<QuestionRequest> questionRequest) {
+        this.questionRequest = questionRequest;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setQuestionRequests(List<QuestionRequest> questionRequests) {
+        this.questionRequest = questionRequests;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+}
